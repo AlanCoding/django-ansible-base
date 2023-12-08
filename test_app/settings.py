ebe0046f3823dc1e0d5590795946eb953b6bebee
Alan Rominger <arominge@redhat.com>
2023-12-07 21:08:25 -0500

    Add an RBAC system as part of DAB app system
    Key features are
     - support of resource tree permission inheritance
     - support for grouping users into teams
     - efficient generation of querysets
     - endpoints for RBAC models, permission classes
    
    Added feature for tracking roles with relationships
    
    Added feature to manage singleton permissions
    
    Add util for assigning permissions in migrations
      add util for creating permissions in custom model
    
    Use null content_type to identify system roles
    
    From review
    Use shared method for add permission determination
    Make the RBAC_checks command more pretty
      give non-zero exit code if issues are identified
    Use " instead of ' for block strings
    Add model to test parent names not matching model

--- test_app/settings.py
+++ test_app/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'ansible_base.rest_filters',
     'ansible_base.jwt_consumer',
     'ansible_base.resource_registry',
+    'ansible_base.rbac',
     'test_app',
     'django_extensions',
     'debug_toolbar',
@@ -114,6 +115,7 @@ DEMO_DATA_COUNTS = {'organization': 150, 'user': 379, 'team': 43}
 
 ANSIBLE_BASE_TEAM_MODEL = 'test_app.Team'
 ANSIBLE_BASE_ORGANIZATION_MODEL = 'test_app.Organization'
+ANSIBLE_BASE_PERMISSION_MODEL = 'auth.Permission'
 
 STATIC_URL = '/static/'
 
@@ -131,3 +133,7 @@ include(settings_file)
 ANSIBLE_BASE_RESOURCE_CONFIG_MODULE = "test_app.resource_api"
 
 SYSTEM_USERNAME = '_system'
+
+ANSIBLE_BASE_ROLE_PRECREATE = {}  # tested in individual tests
+ANSIBLE_BASE_ALLOW_SINGLETON_USER_ROLES = True
+ANSIBLE_BASE_ALLOW_SINGLETON_TEAM_ROLES = True
