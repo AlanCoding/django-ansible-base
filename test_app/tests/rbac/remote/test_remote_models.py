8f04670f876bf4ca3bbf7d8023a6590f5a084dd9
AlanCoding <arominge@redhat.com>
2025-07-01 23:32:22 -0400

    Automatically set DAB types to id of content type to make life easier
    Fix flake8 and simpler type ct calc

--- test_app/tests/rbac/remote/test_remote_models.py
+++ test_app/tests/rbac/remote/test_remote_models.py
@@ -8,9 +8,7 @@ from ansible_base.rbac.remote import RemoteObject
 def test_give_remote_permission(rando):
     foo_type = DABContentType.objects.create(service='foo', model='foo', app_label='foo')
     assert foo_type.service == 'foo'
-    # TODO:
-    # assert foo_type.model_class() is not None
-    foo_foo = DABPermission.objects.create(codename='foo_foo', content_type=foo_type)
+    DABPermission.objects.create(codename='foo_foo', content_type=foo_type)
     rd = RoleDefinition.objects.create_from_permissions(name='Foo fooers for the foos in foo service', permissions=['foo.foo_foo'], content_type=foo_type)
     a_foo = RemoteObject(content_type=foo_type, object_id=42)
     rd.give_permission(rando, a_foo)
