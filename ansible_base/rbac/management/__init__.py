4d1c615b950324a57996efb92c3271bd69e08291
AlanCoding <arominge@redhat.com>
2025-07-02 15:44:05 -0400

    Drop in fix to address broken sequencing

--- ansible_base/rbac/management/__init__.py
+++ ansible_base/rbac/management/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 from django.apps import apps as global_apps
-from django.db import DEFAULT_DB_ALIAS, router
+from django.db import DEFAULT_DB_ALIAS, router, connection
 
 from ansible_base.rbac import permission_registry
 from ansible_base.rbac.remote import get_resource_prefix
@@ -96,3 +96,15 @@ def sync_DAB_permissions(verbosity=2, using=DEFAULT_DB_ALIAS, apps=global_apps):
     Permission.objects.using(using).bulk_create(perms)
     for perm in perms:
         logger.debug("Adding permission '%s'" % perm)
+
+    # Reset the sequence to avoid PK collision later
+    if connection.vendor == 'postgresql':
+        table = DABContentType._meta.db_table
+        pk_column = DABContentType._meta.pk.column
+        with connection.cursor() as cursor:
+            cursor.execute(f"""
+                SELECT setval(
+                    pg_get_serial_sequence(%s, %s),
+                    (SELECT MAX({pk_column}) FROM {table})
+                )
+            """, [table, pk_column])
