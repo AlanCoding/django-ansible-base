73315e22a24bfb93208b7928201fa6e3b45dac25
AlanCoding <arominge@redhat.com>
2025-07-02 20:20:00 -0400

    Use api_slug in serializers

--- ansible_base/rbac/api/serializers.py
+++ ansible_base/rbac/api/serializers.py
@@ -2,11 +2,9 @@ from django.apps import apps
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import transaction
 from django.db.utils import IntegrityError
-from django.utils.functional import cached_property
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 from rest_framework.exceptions import PermissionDenied
-from rest_framework.fields import flatten_choices_dict, to_choices_dict
 from rest_framework.serializers import ValidationError
 
 from ansible_base.lib.abstract_models.common import get_url_for_object
@@ -16,117 +14,29 @@ from ansible_base.rbac.permission_registry import permission_registry  # careful
 from ansible_base.rbac.policies import check_content_obj_permission, visible_users
 from ansible_base.rbac.validators import check_locally_managed, validate_permissions_for_model
 
-from ..remote import get_resource_prefix
-
-
-class ChoiceLikeMixin(serializers.ChoiceField):
-    """
-    This uses a ForeignKey to populate the choices of a choice field.
-    This also manages some string manipulation, right now, adding the local service name.
-    """
-
-    default_error_messages = serializers.PrimaryKeyRelatedField.default_error_messages
-
-    def get_dynamic_choices(self):
-        raise NotImplementedError
-
-    def get_dynamic_object(self, data):
-        raise NotImplementedError
-
-    def to_representation(self, value):
-        raise NotImplementedError
-
-    def __init__(self, **kwargs):
-        # Workaround so that the parent class does not resolve the choices right away
-        self.html_cutoff = kwargs.pop('html_cutoff', self.html_cutoff)
-        self.html_cutoff_text = kwargs.pop('html_cutoff_text', self.html_cutoff_text)
-
-        self.allow_blank = kwargs.pop('allow_blank', False)
-        super(serializers.ChoiceField, self).__init__(**kwargs)
-
-    def _initialize_choices(self):
-        choices = self.get_dynamic_choices()
-        self._grouped_choices = to_choices_dict(choices)
-        self._choices = flatten_choices_dict(self._grouped_choices)
-        self.choice_strings_to_values = {str(k): k for k in self._choices}
-
-    @cached_property
-    def grouped_choices(self):
-        self._initialize_choices()
-        return self._grouped_choices
-
-    @cached_property
-    def choices(self):
-        self._initialize_choices()
-        return self._choices
-
-    def to_internal_value(self, data):
-        try:
-            return self.get_dynamic_object(data)
-        except ObjectDoesNotExist:
-            self.fail('does_not_exist', pk_value=data)
-        except (TypeError, ValueError):
-            self.fail('incorrect_type', data_type=type(data).__name__)
-
-
-class ContentTypeField(ChoiceLikeMixin):
-    def __init__(self, **kwargs):
-        kwargs['help_text'] = _('The type of resource this applies to.')
-        super().__init__(**kwargs)
-
-    def get_resource_type_name(self, cls) -> str:
-        return f"{get_resource_prefix(cls)}.{cls._meta.model_name}"
-
-    def get_dynamic_choices(self):
-        return list(sorted((self.get_resource_type_name(cls), cls._meta.verbose_name.title()) for cls in permission_registry.all_registered_models))
-
-    def get_dynamic_object(self, data):
-        model = data.rsplit('.')[-1]
-        cls = permission_registry.get_model_by_name(model)
-        if cls is None:
-            return permission_registry.content_type_model.objects.none().get()  # raises correct DoesNotExist
-        return permission_registry.content_type_model.objects.get_for_model(cls)
-
-    def to_representation(self, value):
-        if isinstance(value, str):
-            return value  # slight hack to work to AWX schema tests
-        return self.get_resource_type_name(value.model_class())
-
-
-class PermissionField(ChoiceLikeMixin):
-    def get_dynamic_choices(self):
-        perms = []
-        for cls in permission_registry.all_registered_models:
-            cls_name = cls._meta.model_name
-            for action in cls._meta.default_permissions:
-                perms.append(f'{get_resource_prefix(cls)}.{action}_{cls_name}')
-            for perm_name, description in cls._meta.permissions:
-                perms.append(f'{get_resource_prefix(cls)}.{perm_name}')
-        return list(sorted(perms))
-
-    def get_dynamic_object(self, data):
-        codename = data.rsplit('.')[-1]
-        return permission_registry.permission_qs.get(codename=codename)
-
-    def to_representation(self, value):
-        if isinstance(value, str):
-            return value  # slight hack to work to AWX schema tests
-        ct = permission_registry.content_type_model.objects.get_for_id(value.content_type_id)  # optimization
-        return f'{get_resource_prefix(ct.model_class())}.{value.codename}'
-
-
-class ManyRelatedListField(serializers.ListField):
-    def to_representation(self, data):
-        "Adds the .all() to treat the value as a queryset"
-        return [self.child.to_representation(item) if item is not None else None for item in data.all()]
+from ..models import DABContentType, DABPermission
 
 
 class RoleDefinitionSerializer(CommonModelSerializer):
-    # Relational versions - we may switch to these if custom permission and type models are exposed but out of scope here
-    # permissions = serializers.SlugRelatedField(many=True, slug_field='codename', queryset=DABPermission.objects.all())
-    # content_type = ContentTypeField(slug_field='model', queryset=permission_registry.content_type_model.objects.all(), allow_null=True, default=None)
-    permissions = ManyRelatedListField(child=PermissionField())
-    content_type = ContentTypeField(allow_null=True, default=None)
+    permissions = serializers.SlugRelatedField(
+        slug_field='api_slug',
+        queryset=DABPermission.objects.all(),
+        many=True,
+        error_messages={
+            'does_not_exist': "Cannot use permission with api_slug '{value}', object does not exist",
+            'invalid': "Each content type must be a valid slug string",
+        },
+    )
+    content_type = serializers.SlugRelatedField(
+        slug_field='api_slug',
+        queryset=DABContentType.objects.all(),
+        allow_null=True,  # for global roles
+        default=None,
+        error_messages={
+            'does_not_exist': "Cannot use type with api_slug '{value}', object does not exist",
+            'invalid': "Each content type must be a valid slug string",
+        },
+    )
 
     class Meta:
         model = RoleDefinition
@@ -141,7 +51,7 @@ class RoleDefinitionSerializer(CommonModelSerializer):
             permissions = list(self.instance.permissions.all())
         if 'content_type' in validated_data:
             content_type = validated_data['content_type']
-        else:
+        elif self.instance:
             content_type = self.instance.content_type
         validate_permissions_for_model(permissions, content_type)
         if getattr(self, 'instance', None):
@@ -150,11 +60,11 @@ class RoleDefinitionSerializer(CommonModelSerializer):
 
 
 class RoleDefinitionDetailSerializer(RoleDefinitionSerializer):
-    content_type = ContentTypeField(read_only=True)
+    content_type = serializers.SlugRelatedField(slug_field='api_slug', read_only=True)
 
 
 class BaseAssignmentSerializer(CommonModelSerializer):
-    content_type = ContentTypeField(read_only=True, allow_null=True)
+    content_type = serializers.SlugRelatedField(slug_field='api_slug', read_only=True)
     object_ansible_id = serializers.UUIDField(
         required=False,
         help_text=_('The resource id of the object this role applies to. An alternative to the object_id field.'),
