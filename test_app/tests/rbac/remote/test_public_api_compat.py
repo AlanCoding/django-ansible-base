e7ec1aa6f0f5344be225516affabacdd1d15fb95
AlanCoding <arominge@redhat.com>
2025-07-03 10:25:25 -0400

    Make assignment list fully function with remote permissions

--- test_app/tests/rbac/remote/test_public_api_compat.py
+++ test_app/tests/rbac/remote/test_public_api_compat.py
@@ -1,6 +1,7 @@
 import pytest
 
 from ansible_base.lib.utils.response import get_relative_url
+from ansible_base.rbac.remote import RemoteObject
 
 
 @pytest.mark.django_db
@@ -30,26 +31,25 @@ def test_create_remote_role_definition(admin_api_client, foo_type, foo_permissio
     assert response.data['permissions'] == ['foo.foo_foo']
 
 
-# TODO: check that assignment endpoint works
+@pytest.mark.django_db
+def test_give_remote_permission(admin_api_client, rando, foo_type, foo_rd):
+    a_foo = RemoteObject(content_type=foo_type, object_id=42)
+    assignment = foo_rd.give_permission(rando, a_foo)
+    assignment.content_object
 
-# @pytest.mark.django_db
-# def test_give_remote_permission(rando, foo_type, foo_permission, foo_rd):
-#     assert foo_type.service == 'foo'  # a place, a domain, a server, known as foo
-#     assert foo_type.api_slug == 'foo.foo'  # there lives a foo in foo
+    assert isinstance(assignment.content_object, RemoteObject)
 
-#     assert foo_permission.api_slug == 'foo.foo_foo'  # expression of the ability that one may foo a foo
+    # Should show up in the assignments list
+    url = get_relative_url('roleuserassignment-list')
+    response = admin_api_client.get(url, format="json")
+    assert response.status_code == 200, response.data
 
-#     a_foo = RemoteObject(content_type=foo_type, object_id=42)
-#     assignment = foo_rd.give_permission(rando, a_foo)
-
-#     assignment = RoleUserAssignment.objects.get(pk=assignment.pk)
-#     assert isinstance(assignment.content_object, RemoteObject)
-
-#     # We can do evaluation querysets, but these can not return objects, just id values
-#     assert set(foo_type.model_class().access_ids_qs(actor=rando, codename='foo')) == {(int(assignment.object_id),)}
-
-#     # Test that user-attached methods also work
-#     assert rando.has_obj_perm(a_foo, 'foo')
-#     with pytest.raises(RuntimeError) as exc:
-#         assert not rando.has_obj_perm(a_foo, 'bar')  # not a valid permission
-#     assert 'The permission bar_foo is not valid for model foo' in str(exc)
+    data_by_rd = {item['role_definition']: item for item in response.data['results']}
+    assert foo_rd.id in data_by_rd
+    item = data_by_rd[foo_rd.id]
+    assert item['user'] == rando.id
+    assert item['object_id'] == str(a_foo.object_id)
+    assert 'summary_fields' in item
+    sf = item['summary_fields']
+    assert 'content_object' in sf
+    assert sf['content_object'] == {'<remote_object_placeholder>': True, 'model_name': 'foo', 'service': 'foo', 'pk': 42}
--- test_app/tests/rbac/remote/test_remote_assignment.py
+++ test_app/tests/rbac/remote/test_remote_assignment.py
@@ -2,6 +2,7 @@ import pytest
 
 from ansible_base.rbac.models import RoleUserAssignment
 from ansible_base.rbac.remote import RemoteObject
+from test_app.models import User
 
 
 @pytest.mark.django_db
@@ -13,9 +14,12 @@ def test_give_remote_permission(rando, foo_type, foo_permission, foo_rd):
 
     a_foo = RemoteObject(content_type=foo_type, object_id=42)
     assignment = foo_rd.give_permission(rando, a_foo)
+    assert '42' in repr(a_foo)
 
     assignment = RoleUserAssignment.objects.get(pk=assignment.pk)
+    assert assignment.content_object.pk == 42
     assert isinstance(assignment.content_object, RemoteObject)
+    assert isinstance(assignment.content_object, RemoteObject)  # There was a bug where multiple references would error
 
     # We can do evaluation querysets, but these can not return objects, just id values
     assert set(foo_type.model_class().access_ids_qs(actor=rando, codename='foo')) == {(int(assignment.object_id),)}
@@ -25,3 +29,18 @@ def test_give_remote_permission(rando, foo_type, foo_permission, foo_rd):
     with pytest.raises(RuntimeError) as exc:
         assert not rando.has_obj_perm(a_foo, 'bar')  # not a valid permission
     assert 'The permission bar_foo is not valid for model foo' in str(exc)
+
+
+@pytest.mark.django_db
+def test_prefetch_related_objects(foo_type, foo_rd, inv_rd, inventory):
+    users = [User.objects.create(username=f'user{i}') for i in range(10)]
+
+    a_foo = RemoteObject(content_type=foo_type, object_id=42)
+    for u in users:
+        foo_rd.give_permission(u, a_foo)
+        inv_rd.give_permission(u, inventory)
+
+    assert RoleUserAssignment.objects.count() == 20
+    assert {assignment.content_object for assignment in RoleUserAssignment.objects.all()} == {a_foo, inventory}
+    assert {assignment.content_object for assignment in RoleUserAssignment.objects.all()} == {a_foo, inventory}
+    assert {assignment.content_object for assignment in RoleUserAssignment.objects.prefetch_related('content_object')} == {a_foo, inventory}
