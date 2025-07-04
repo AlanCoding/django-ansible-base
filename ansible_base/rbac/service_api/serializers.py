4197a011f4ca4a2754d6bd4714d49735e74d5c7e
AlanCoding <arominge@redhat.com>
2025-07-04 07:13:50 -0400

    Add user and team sync endpoints

--- ansible_base/rbac/service_api/serializers.py
+++ ansible_base/rbac/service_api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
-from ..models import DABContentType, DABPermission
+from ..models import DABContentType, DABPermission, RoleTeamAssignment, RoleUserAssignment
+from ..remote import RemoteObject
 
 
 class DABContentTypeSerializer(serializers.ModelSerializer):
@@ -17,3 +18,46 @@ class DABPermissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = DABPermission
         fields = ['api_slug', 'codename', 'content_type', 'name']
+
+
+assignment_common_fields = ('created', 'created_by_ansible_id', 'object_id', 'object_ansible_id', 'content_type', 'role_definition')
+
+
+class BaseAssignmentSerializer(serializers.ModelSerializer):
+    content_type = serializers.SlugRelatedField(read_only=True, slug_field='api_slug')
+    role_definition = serializers.SlugRelatedField(read_only=True, slug_field='name')
+    created_by_ansible_id = serializers.SerializerMethodField()
+    object_ansible_id = serializers.SerializerMethodField()
+
+    def get_created_by_ansible_id(self, obj):
+        return str(obj.created_by.resource.ansible_id)
+
+    def get_object_ansible_id(self, obj):
+        content_object = obj.content_object
+        if isinstance(content_object, RemoteObject):
+            return None
+        if hasattr(content_object, 'resource'):
+            return str(content_object.resource.ansible_id)
+        return None
+
+
+class RoleUserAssignmentSerializer(BaseAssignmentSerializer):
+    user_ansible_id = serializers.SerializerMethodField()
+
+    class Meta:
+        model = RoleUserAssignment
+        fields = assignment_common_fields + ('user_ansible_id',)
+
+    def get_user_ansible_id(self, obj):
+        return str(obj.user.resource.ansible_id)
+
+
+class RoleTeamAssignmentSerializer(BaseAssignmentSerializer):
+    user_ansible_id = serializers.SerializerMethodField()
+
+    class Meta:
+        model = RoleTeamAssignment
+        fields = assignment_common_fields + ('team_ansible_id',)
+
+    def get_team_ansible_id(self, obj):
+        return str(obj.team.resource.ansible_id)
