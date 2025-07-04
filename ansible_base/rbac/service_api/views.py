24f887a60d093c21206625d3becb11e3095686bc
AlanCoding <arominge@redhat.com>
2025-07-04 07:13:50 -0400

    Add user and team sync endpoints

--- ansible_base/rbac/service_api/views.py
+++ ansible_base/rbac/service_api/views.py
@@ -2,7 +2,7 @@ from rest_framework.viewsets import GenericViewSet, mixins
 
 from ansible_base.lib.utils.views.django_app_api import AnsibleBaseDjangoAppApiView
 
-from ..models import DABContentType, DABPermission
+from ..models import DABContentType, DABPermission, RoleTeamAssignment, RoleUserAssignment
 from . import serializers as service_serializers
 
 
@@ -26,3 +26,31 @@ class RolePermissionTypeViewSet(
 
     queryset = DABPermission.objects.prefetch_related('content_type').all()
     serializer_class = service_serializers.DABPermissionSerializer
+
+
+# NOTE: role definitions are exchanged via the resources endpoint, so not included here
+
+
+prefetch_related = ('created_by__resource', 'content_type', 'role_definition')
+
+
+class ServiceRoleUserAssignmentViewSet(
+    AnsibleBaseDjangoAppApiView,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    """List of assignments for cross-service communication"""
+
+    queryset = RoleUserAssignment.objects.prefetch_related('user__resource', *prefetch_related)
+    serializer_class = service_serializers.RoleUserAssignmentSerializer
+
+
+class ServiceRoleTeamAssignmentViewSet(
+    AnsibleBaseDjangoAppApiView,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    """List of team role assignments for cross-service communication"""
+
+    queryset = RoleTeamAssignment.objects.prefetch_related('team__resource', *prefetch_related)
+    serializer_class = service_serializers.RoleTeamAssignmentSerializer
