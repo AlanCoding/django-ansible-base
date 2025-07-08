495ef82ecb7a0e39a180ce6ff3a8ec27dedbcfec
AlanCoding <arominge@redhat.com>
2025-07-08 00:17:00 -0400

    First pass at rest of views

--- ansible_base/rbac/urls.py
+++ ansible_base/rbac/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 
 from ansible_base.rbac.api.router import router
-from ansible_base.rbac.api.views import RoleMetadataView
+from ansible_base.rbac.api.views import RoleMetadataView, TeamAccessViewSet, UserAccessViewSet
 from ansible_base.rbac.apps import AnsibleRBACConfig
 
 from .service_api.router import service_router
@@ -13,10 +13,15 @@ service_urls = [
     path('', include(service_router.urls)),
 ]
 
+user_access_view = UserAccessViewSet.as_view({'get': 'list'})
+team_access_view = TeamAccessViewSet.as_view({'get': 'list'})
+
 api_version_urls = [
     path('', include(router.urls)),
     path('service-index/', include(service_urls)),
     path(r'role_metadata/', RoleMetadataView.as_view(), name="role-metadata"),
+    path('role_user_access/<str:model_name>/<int:pk>/', user_access_view, name="role-user-access"),
+    path('role_team_access/<str:model_name>/<int:pk>/', team_access_view, name="role-team-access"),
 ]
 
 root_urls = []
