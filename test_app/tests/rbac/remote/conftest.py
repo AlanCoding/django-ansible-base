38d8d17951ca3a578a7bdd34b38437bea46777eb
AlanCoding <arominge@redhat.com>
2025-07-14 10:57:13 -0400

    Test and fix creating assignments through the API

--- test_app/tests/rbac/remote/conftest.py
+++ test_app/tests/rbac/remote/conftest.py
@@ -19,3 +19,18 @@ def foo_rd(foo_type, foo_permission):
     return RoleDefinition.objects.create_from_permissions(
         name='Foo fooers for the foos in foo service', permissions=[foo_permission.api_slug], content_type=foo_type
     )
+
+
+@pytest.fixture
+def foo_type_uuid():
+    return DABContentType.objects.create(service='foo', model='foo_uuid', app_label='foo', pk_field_type='uuid')
+
+
+@pytest.fixture
+def foo_permission_uuid(foo_type_uuid):
+    return DABPermission.objects.create(codename='foo_foo_uuid', content_type=foo_type_uuid)
+
+
+@pytest.fixture
+def foo_rd_uuid(foo_type_uuid, foo_permission_uuid):
+    return RoleDefinition.objects.create_from_permissions(name='UUID foo role thing', permissions=[foo_permission_uuid.api_slug], content_type=foo_type_uuid)
