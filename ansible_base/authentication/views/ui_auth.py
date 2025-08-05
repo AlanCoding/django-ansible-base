a0f40383ae135a115edbc1a09baf6647befa602e
Bryan Havenstein <49794041+bhavenst@users.noreply.github.com>
2025-08-05 10:04:21 -0600

    AAP-49728 Allow absolute paths for LOGIN_REDIRECT_OVERRIDE (#784)
    LOGIN_REDIRECT_OVERRIDE can be an absolute path or a URL.

--- ansible_base/authentication/views/ui_auth.py
+++ ansible_base/authentication/views/ui_auth.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Union
 
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
@@ -8,7 +9,7 @@ from rest_framework.serializers import ValidationError
 from ansible_base.authentication.models import Authenticator
 from ansible_base.authentication.serializers import UIAuthResponseSerializer
 from ansible_base.lib.utils.settings import get_setting, is_aoc_instance
-from ansible_base.lib.utils.validation import validate_image_data, validate_url
+from ansible_base.lib.utils.validation import validate_absolute_path, validate_image_data, validate_url
 from ansible_base.lib.utils.views.django_app_api import AnsibleBaseDjangoAppApiView
 
 logger = logging.getLogger('ansible_base.authentication.views.ui_auth')
@@ -37,6 +38,23 @@ class UIAuth(AnsibleBaseDjangoAppApiView):
 
         def get(self):
             return self._get()
+
+
+def _validate_and_get_login_redirect_override() -> Union[str, None]:
+    try:
+        login_redirect_override = get_setting('LOGIN_REDIRECT_OVERRIDE', '')
+        # ignore validation if login_redirect_override is None or empty string
+        if login_redirect_override is not None and login_redirect_override != '':
+            validate_url(url=login_redirect_override, schemes=['https', 'http'], allow_plain_hostname=True)
+            return login_redirect_override
+    except ValidationError:
+        # login_redirect_override can also be an absolute path
+        try:
+            validate_absolute_path(path=login_redirect_override)
+            return login_redirect_override
+        except ValidationError:
+            logger.error('LOGIN_REDIRECT_OVERRIDE was set but was not a valid URL or absolute path, ignoring')
+    return None
 
 
 def generate_ui_auth_data():
@@ -68,14 +86,9 @@ def generate_ui_auth_data():
         else:
             logger.error(f"Don't know how to handle authenticator of type {authenticator.type}")
 
-    try:
-        login_redirect_override = get_setting('LOGIN_REDIRECT_OVERRIDE', '')
-        # ignore validation if login_redirect_override is None or empty string
-        if login_redirect_override is not None and login_redirect_override != '':
-            validate_url(url=login_redirect_override, allow_plain_hostname=True)
-            response['login_redirect_override'] = login_redirect_override
-    except ValidationError:
-        logger.error('LOGIN_REDIRECT_OVERRIDE was set but was not a valid URL, ignoring')
+    login_redirect_override = _validate_and_get_login_redirect_override()
+    if login_redirect_override:
+        response['login_redirect_override'] = login_redirect_override
 
     custom_login_info = get_setting('custom_login_info', '')
     if isinstance(custom_login_info, str):
