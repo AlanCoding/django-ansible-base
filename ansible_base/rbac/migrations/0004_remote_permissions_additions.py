b1e9307a628a0542ad8d36e482446c05da7351df
AlanCoding <arominge@redhat.com>
2025-09-10 13:29:17 -0400

    Move object_created to new migration step

--- ansible_base/rbac/migrations/0004_remote_permissions_additions.py
+++ ansible_base/rbac/migrations/0004_remote_permissions_additions.py
@@ -160,28 +160,6 @@ class Migration(migrations.Migration):
             model_name='roleevaluationuuid',
             name='dab_rbac_ro_role_id_4fe905_idx',
         ),
-        # Added field for a checksum like purpose for remote models
-        migrations.AddField(
-            model_name='roleuserassignment',
-            name='object_created',
-            field=models.DateTimeField(help_text='The created timestamp of related object, if applicable.', null=True),
-        ),
-        migrations.AddField(
-            model_name='roleteamassignment',
-            name='object_created',
-            field=models.DateTimeField(help_text='The created timestamp of related object, if applicable.', null=True),
-        ),
-        # Make assignment created timestamp backdateable
-        migrations.AlterField(
-            model_name='roleteamassignment',
-            name='created',
-            field=models.DateTimeField(default=django.utils.timezone.now, editable=False, help_text='The date/time this resource was created.'),
-        ),
-        migrations.AlterField(
-            model_name='roleuserassignment',
-            name='created',
-            field=models.DateTimeField(default=django.utils.timezone.now, editable=False, help_text='The date/time this resource was created.'),
-        ),
         # Fields unique to DAB RBAC and not generally shared with ContentType or Permission
         migrations.AddField(
             model_name='dabcontenttype',
