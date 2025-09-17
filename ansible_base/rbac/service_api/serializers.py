f22861c36f1427e270055ba688a84657409095f0
AlanCoding <arominge@redhat.com>
2025-09-17 14:12:34 -0400

    flake8

--- ansible_base/rbac/service_api/serializers.py
+++ ansible_base/rbac/service_api/serializers.py
@@ -1,5 +1,4 @@
 from crum import impersonate
-from django.apps import apps
 from django.db import transaction
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
