8a70b9dc8b4dd1dc3b130e936aba3a7b91b42ba6
AlanCoding <arominge@redhat.com>
2025-09-18 14:03:25 -0400

    Revert file change

--- ansible_base/lib/templatetags/requests.py
+++ ansible_base/lib/templatetags/requests.py
@@ -1,19 +1,10 @@
+from django import template
+
 from ansible_base.lib.utils import requests as dab_requests
 
-# Global variable to hold the Django template library
-register = None
+register = template.Library()
 
 
+@register.simple_tag
 def is_proxied_request():
     return dab_requests.is_proxied_request()
-
-
-# Auto-register when imported in Django context
-try:
-    from django import template
-
-    register = template.Library()
-    register.simple_tag(is_proxied_request)
-except ImportError:
-    # Django not available, defer registration
-    pass
