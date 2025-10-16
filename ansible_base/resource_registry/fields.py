f573bcde63bac56b84dac14a163c14ca5a673707
AlanCoding <arominge@redhat.com>
2025-10-16 13:55:48 -0400

    Revert "Also exclude from coverage"
    This reverts commit 0f978c62c0cf509079dc84e6ca41f3bf5c79429a.

--- ansible_base/resource_registry/fields.py
+++ ansible_base/resource_registry/fields.py
@@ -48,7 +48,7 @@ class CustomForwardOneToOneDescriptor(ForwardOneToOneDescriptor):
             False,
         )
 
-    def get_prefetch_queryset(self, instances, queryset=None):  # NOSONAR  # pragma: no cover
+    def get_prefetch_queryset(self, instances, queryset=None):  # NOSONAR
         # Django 4 compatibility: renamed to get_prefetch_querysets in Django 5
         if queryset is None:
             return self.get_prefetch_querysets(instances)
